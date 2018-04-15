7e4d13f23d0d938c352ac1630f7050dbb515ce8a
hyunwoo.shim <shim.hw0810@gmail.com>
2018-04-15 15:47:15 +0900

    Modified test

--- main/src/androidTest/java/xyz/laziness/fontsimulator/main/ExampleInstrumentedTest.kt
+++ main/src/androidTest/java/xyz/laziness/fontsimulator/main/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package xyz.laziness.fontsimulator.main
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("xyz.laziness.fontsimulator.feature.test", appContext.packageName)
+        assertEquals("xyz.laziness.fontsimulator.main.test", appContext.packageName)
     }
 }
