8a489ebb6e24961e76e84f38f2277098f73bb204
hyunwoo.shim <shim.hw0810@gmail.com>
2018-07-26 17:24:13 +0900

    Request] Change textSize unit sp to dp

--- main/src/main/java/xyz/laziness/fontsimulator/main/MainActivity.kt
+++ main/src/main/java/xyz/laziness/fontsimulator/main/MainActivity.kt
@@ -59,9 +59,9 @@ class MainActivity : MyActivity() {
             override fun onProgressChanged(sb: SeekBar?, progress: Int, fromUser: Boolean) {
                 val currentSize = (progress + STANDARD_TEXT_SIZE).toFloat()
                 singleLineText.setTextSize(
-                        TypedValue.COMPLEX_UNIT_SP, currentSize)
+                        TypedValue.COMPLEX_UNIT_DIP, currentSize)
                 multiLineText.setTextSize(
-                        TypedValue.COMPLEX_UNIT_SP, currentSize)
+                        TypedValue.COMPLEX_UNIT_DIP, currentSize)
                 fontSizeValue.text = String.format(
                         getString(R.string.sp_holder), currentSize.toInt())
             }
@@ -144,7 +144,7 @@ class MainActivity : MyActivity() {
 
     private fun initValueView() {
         fontSizeValue.text = String.format(
-                getString(R.string.sp_holder), STANDARD_TEXT_SIZE)
+                getString(R.string.dp_holder), STANDARD_TEXT_SIZE)
         lineSpacingValue.text = String.format(
                 getString(R.string.px_under_two_holder), 0f)
         letterSpacingValue.text = String.format(
